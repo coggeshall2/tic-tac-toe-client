f1c203c3edd7863b2e1026ddd910227be7e6bbd5
coggeshall2 <roncogg@hotmail.com>
2018-06-24 16:16:09 -0400

    Created a function tic tac toe board I now get an alert message for win or lose, nothing for tie Bug-alert message is received before final mark is appended to board

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,19 +10,59 @@ $(() => {
 
 });
 
-const board = ['topleft', 'topcenter', 'topright', 'leftcenter', 'middle', 'rightcenter', 'bottomleft', 'bottomcenter', 'bottomright'];
-//let player1 = Player('Player 1', '<i class="fas fa-times"></i>');
-//let player2 = Player('Player 2', '<i class="far fa-circle"></i>');
 
-//console log shows me each square is targeted correctly
-  let theParent = document.querySelector(".board");
-  theParent.addEventListener("click", markSquare, false);
-
-  function markSquare(square) {
-      if (square.target !== square.currentTarget) {
-          let clickedSquare = square.target.id;
-          console.log("Hello " + clickedSquare);
-        }
-      };
+// const board = ['topleft', 'topcenter', 'topright', 'leftcenter', 'middle', 'rightcenter', 'bottomleft', 'bottomcenter', 'bottomright'];
+// //let player1 = Player('Player 1', '<i class="fas fa-times"></i>');
+// //let player2 = Player('Player 2', '<i class="far fa-circle"></i>');
+//
+//   let theParent = document.querySelector(".board");
+//   theParent.addEventListener("click", markSquare, false);
+//    //console log shows me each square is targeted correctly
+//
+//
+//     function markSquare(square) {
+//       if (square.target !== square.currentTarget) {
+//           let clickedSquare = square.target.id;
+//           console.log("Hello " + clickedSquare);
+//                   console.log("x");
+//         }
+//      };
 
 //mark square with an x if player 1 or an o if player 2
+
+var move = 1;
+ var turn = true;
+ $("#board tr td").click(function() {
+   if ($(this).text()=="" && turn) {
+     if ((move%2)==1) { $(this).append("X"); }
+     else { $(this).append("O"); }
+     move++;
+     if (ForWinner()!=-1 && ForWinner()!="") {
+           if (ForWinner()=="X") { alert("Player 1 wins!"); }
+           else { alert("Player 2 wins!"); }
+           turn = false;
+         }
+        }
+       });
+
+
+   function ForWinner() {
+   var field2 = $("#board tr:nth-child(1) td:nth-child(2)").text();
+   var field3 = $("#board tr:nth-child(1) td:nth-child(3)").text();
+   var field1 = $("#board tr:nth-child(1) td:nth-child(1)").text();
+   var field4 = $("#board tr:nth-child(2) td:nth-child(1)").text();
+   var field5 = $("#board tr:nth-child(2) td:nth-child(2)").text();
+   var field6 = $("#board tr:nth-child(2) td:nth-child(3)").text();
+   var field7 = $("#board tr:nth-child(3) td:nth-child(1)").text();
+   var field8 = $("#board tr:nth-child(3) td:nth-child(2)").text();
+   var field9 = $("#board tr:nth-child(3) td:nth-child(3)").text();
+   if  ((field1==field2)&&(field2==field3)) { return field3; }
+   else if ((field4==field5)&&(field5==field6)) { return field6; }
+   else if ((field7==field8)&&(field8==field9)) { return field9; }
+   else if ((field1==field4)&&(field4==field7)) { return field7; }
+   else if ((field2==field5)&&(field5==field8)) { return field8; }
+   else if ((field3==field6)&&(field6==field9)) { return field9; }
+   else if ((field1==field5)&&(field5==field9)) { return field9; }
+   else if ((field3==field5)&&(field5==field7)) { return field7; }
+   return -1;
+ }
