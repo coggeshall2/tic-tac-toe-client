7a75020efe81fc74fdb41d07f4549f3b4a606aef
coggeshall2 <roncogg@hotmail.com>
2018-06-26 21:11:20 -0400

    Adjusted CSS styles Tinkered with index.js with no favorable results

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,21 +9,23 @@
 
 const authEvents = require('./events.js')
 
-// On document ready
+
+
+//On document ready
 $(() => {
  authEvents.addHandlers()
-})
-// let startGame = function(auth){
-//   if (signUpSuccess(data) == null &&
-//       signInSuccess(data) == null){
-//         turn = false}
-//         else {turn = true;}
-//       }
+});
+
+
+
+
+
+
 
 //mark square with an x if player 1 or an o if player 2
-// set moves to 1 and turn to true
-var move = 1;
- var turn = true;
+// set moves counter to 1 and turn to true for player 1
+ let move = 1;
+ let turn = true;
  $("#board tr td").click(function() {
    // if square is empty and turn is true continue
    if ($(this).text()=="" && turn)
@@ -32,17 +34,25 @@ var move = 1;
   // else it is player 2 so mark an o
      else { $(this).append("O"); }
   // add 1 to move total when total reaches 9 game will be a tie
-     move++;
+    move++;
+    //if moves is 10 then all squares are full so its a tie
      if(move == 10) {alert("Its adraw")}
+     // if forwinner is not equal to -1 and forwinner is not blank
         if (ForWinner()!=-1 && ForWinner()!="") {
+      // if winning combo is x then player 1 wins
             if (ForWinner()=="X") { alert("Player 1 wins!"); }
+      // if winning combo is o then player 2 wins
             else { alert("Player 2 wins!"); }
+      // set turn to false for next player 2 turn
             turn = false;
           };
         });
 
+
+
   // check for winner
    function ForWinner() {
+     // setting each square to a variable
    let field1 = $("#board tr:nth-child(1) td:nth-child(1)").text();
    let field2 = $("#board tr:nth-child(1) td:nth-child(2)").text();
    let field3 = $("#board tr:nth-child(1) td:nth-child(3)").text();
@@ -52,6 +62,7 @@ var move = 1;
    let field7 = $("#board tr:nth-child(3) td:nth-child(1)").text();
    let field8 = $("#board tr:nth-child(3) td:nth-child(2)").text();
    let field9 = $("#board tr:nth-child(3) td:nth-child(3)").text();
+   // checking each winning combo for true state
    if  ((field1==field2)&&(field2==field3)) { return field3; }
    else if ((field4==field5)&&(field5==field6)) { return field6; }
    else if ((field7==field8)&&(field8==field9)) { return field9; }
@@ -60,5 +71,6 @@ var move = 1;
    else if ((field3==field6)&&(field6==field9)) { return field9; }
    else if ((field1==field5)&&(field5==field9)) { return field9; }
    else if ((field3==field5)&&(field5==field7)) { return field7; }
+   // returns -1 if no winning combo exists
    return -1;
  }
