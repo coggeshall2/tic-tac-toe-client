772708405a9d8867e3f9b9b2dcb387f8a42a3a04
coggeshall2 <roncogg@hotmail.com>
2018-07-04 15:53:56 -0400

    resolving errors

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,8 +1,6 @@
 'use strict'
-const getFormFields = require(`../../lib/get-form-fields`)
-const events = require('./events')
-const api = require('./api')
-const ui = require('./ui')
+
+
 const store = {
 }
 
